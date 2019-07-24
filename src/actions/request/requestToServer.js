bd900957118902d82ff92c5aac29f22de5b0370f
Dmitry Petrov <dmitrypetrov93gmail.com>
2019-07-24 15:30:19 +0300

    update: change '/login' request method to POST

--- src/actions/request/requestToServer.js
+++ src/actions/request/requestToServer.js
@@ -8,12 +8,17 @@ export function requestToServer(userData) {
     return (dispatch) => {
         dispatch(requestIsLoading());
         axios
-        .get('/login?userName=' + userData.userName + '&password=' + userData.password)
-        .then(response => {
-            dispatch(requestSuccessed(response.data));
-        })
-        .catch(function (error) {
-            dispatch(requestFailed(error.message));
-        });
+            .post(
+                '/login', 
+                {
+                    userName: userData.userName,
+                    password: userData.password
+                })
+            .then(response => {
+                dispatch(requestSuccessed(response.data));
+            })
+            .catch(function (error) {
+                dispatch(requestFailed(error.message));
+            });
     };
 }
