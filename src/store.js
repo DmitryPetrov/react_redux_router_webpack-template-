95d5fd6decb635443c2579e1b021428bd22ffd28
Dmitry Petrov <dmitrypetrov93gmail.com>
2019-07-24 12:12:02 +0300

    fix: initial state object name

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ const initialState = {
     userName: '(initialState)',
     password: '(initialState)',
   },
-  response: {
+  request: {
     isFail: '(initialState)',
     isLoading: '(initialState)',
     isSuccessed: '(initialState)',
