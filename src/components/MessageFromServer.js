3491541f9031728e6cef75442185f72f8038c732
Dmitry Petrov <dmitrypetrov93gmail.com>
2019-10-28 17:52:15 +0300

    update: material_ui style to component MessageFromServer [scope:component src/components/MessageFromServer.js] [RU - обновил компонент MessageFromServer в стиле material_ui]

--- src/components/MessageFromServer.js
+++ src/components/MessageFromServer.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-import SoapMessageList from './SoapMessageList'
-import ObjectFromServer from './ObjectFromServer'
+import { GLOBAL_STYLE, CONTAINER_MAX_WIDTH } from './../style';
 
 const useStyles = makeStyles(theme => ({
   paper: {
+    marginTop: theme.spacing(3),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -14,43 +17,56 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(2),
   },
   typography_error: {
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(7),
     color: theme.palette.error.dark,
+  },
+  typography_ok: {
+    marginTop: theme.spacing(7),
   }
 }));
 
-function MessageFromServer(props) {
+function AuthorizationResponse(props) {
   const classes = useStyles();
+  const globalStyle = GLOBAL_STYLE();
 
   if (props.request.isFail === true) {
     return (
-      <div className={classes.paper}>
-        <h2>Request status: {props.request.message}</h2>
+      <div className={globalStyle.paper}>
+        <Container component="main" maxWidth={CONTAINER_MAX_WIDTH} className={classes.paper}>
+          <Typography component="h1" variant="h5" className={classes.typography_error}>
+            Failed
+          </Typography>
+        </Container>
       </div>
-      );
+    );
   }
 
   if (props.request.isLoading === true) {
     return (
-      <div className={classes.paper}>
-        <h2>Request status: {props.request.message}</h2>
+      <div className={globalStyle.paper}>
+        <Container component="main" maxWidth={CONTAINER_MAX_WIDTH} className={classes.paper}>
+          <CircularProgress disableShrink />
+          <Typography component="h1" variant="h5" className={classes.typography}>
+            Loading
+          </Typography>
+        </Container>
       </div>
-      );
+     );
   }
 
   if (props.request.isSuccessed === true) {
     return (
-      <div className={classes.paper}>
-        <h2>Request status: {props.request.message}</h2>
-        <h2>Server response status: {props.request.response.status}</h2>
-        <h2>Message from server: {props.request.response.message}</h2>
-        <SoapMessageList list={props.request.response.soapMessageList}/>
-        <ObjectFromServer object={props.request.response.requestChain}/>
+      <div className={globalStyle.paper}>
+        <Container component="main" maxWidth={CONTAINER_MAX_WIDTH} className={classes.paper}>
+          <Typography component="h1" variant="h5" className={classes.typography_ok}>
+            {props.request.response.message}
+          </Typography>
+        </Container>
       </div>
-      );
+    );
   }
 
-  return <div></div>;
+  return (<div className="AuthorizationResponse"></div>);
 }
 
-export default MessageFromServer;
+export default AuthorizationResponse;
