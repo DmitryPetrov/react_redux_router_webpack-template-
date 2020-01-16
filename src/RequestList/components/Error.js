2bf08a2fcaa655a9340e28a112050dc2d970df6d
Dmitry Petrov <dmitrypetrov93gmail.com>
2020-01-16 14:40:18 +0300

    fix: soap message can go out of component border [scope:component */RequestList/components/Error.js] [RU - исправил, soap сообщение могло выходить за границы компонента]

--- src/RequestList/components/Error.js
+++ src/RequestList/components/Error.js
@@ -4,25 +4,33 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import { makeStyles } from '@material-ui/core/styles';
 
 import SoapMessageList from './../../components/SoapMessageList';
 import { REQUEST_STYLE } from './../style';
 
+const useStyles = makeStyles(theme => ({
+  width: {
+    width: '100%',
+  },
+}));
+
 function Error(props)  {
-  const classes = REQUEST_STYLE();
+  const globalStyle = REQUEST_STYLE();
+  const classes = useStyles();
 
   return (
-    <ExpansionPanel className={classes.expansionPanelError}>
+    <ExpansionPanel className={globalStyle.expansionPanelError}>
       <ExpansionPanelSummary
         expandIcon={<ExpandMoreIcon />}
         aria-controls="panel1a-content"
         id="panel1a-header"
       >
-        <Typography className={classes.heading}>ERROR!!!</Typography>
+        <Typography className={globalStyle.heading}>ERROR!!!</Typography>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
-	      <div>
-	      	<Typography className={classes.heading}>Server message: {props.message}</Typography>
+	      <div className={classes.width}>
+	      	<Typography className={globalStyle.heading}>Server message: {props.message}</Typography>
 	        <SoapMessageList list={props.xmlList}/>
         </div>
       </ExpansionPanelDetails>
